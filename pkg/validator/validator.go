8eb1da6fb675ee9df4351e8f1e1a15536ffecb5f
Kravchuk Roman <kravchuk.roman02@gmail.com>
2023-10-17 14:22:09 +0800

    implement struct validator

---
+++ pkg/validator/validator.go
@@ -0,0 +1,66 @@
+package validator
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	ent "github.com/go-playground/validator/v10/translations/en"
+)
+
+type Validator struct {
+	validator  *validator.Validate
+	translator ut.Translator
+
+	errors map[string]string
+	mu     sync.RWMutex
+}
+
+func New() (*Validator, error) {
+	var (
+		v             = validator.New()
+		enTranslator  = en.New()
+		translator, _ = ut.New(enTranslator, enTranslator).GetTranslator("en")
+	)
+
+	err := ent.RegisterDefaultTranslations(v, translator)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Validator{
+		errors:     make(map[string]string),
+		validator:  v,
+		translator: translator,
+	}, nil
+}
+
+func (v *Validator) Valid(input interface{}) bool {
+	var (
+		errs validator.ValidationErrors
+		err  = v.validator.Struct(input)
+	)
+
+	if err != nil {
+		if errors.As(err, &errs) {
+			for _, e := range errs {
+				v.mu.Lock()
+				v.errors[e.Field()] = e.Translate(v.translator)
+				v.mu.Unlock()
+			}
+		}
+
+		return false
+	}
+
+	return true
+}
+
+func (v *Validator) Errors() map[string]string {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	return v.errors
+}
